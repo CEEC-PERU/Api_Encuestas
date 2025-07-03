9f5a8134e6771a6368e7ffcd711d07e7434f2bbb
ceec <administrador.app@ceec.com.pe>
2025-07-03 17:29:23 -0500

    updated-bd

--- main.py
+++ main.py
@@ -2,20 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-import psycopg2
-
-try:
-    conn = psycopg2.connect(
-        host="mininazem.cj8iguu6cni6.us-east-1.rds.amazonaws.com",
-        dbname="nombre_bd",
-        user="usuario",
-        password="contraseña",
-        port=5432
-    )
-    print("¡Conexión exitosa!")
-    conn.close()
-except Exception as e:
-    print(f"Error: {e}")
 
 @app.get("/")
 async def root():
